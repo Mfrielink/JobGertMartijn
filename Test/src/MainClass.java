adcb4d8ee244c727a7d989703edd6e7bb935c679
JobbD <jobdouma@hotmail.com>
2014-05-08 11:34:12 +0200

    test

--- Test/src/MainClass.java
+++ Test/src/MainClass.java
@@ -1,10 +1,19 @@
-
+import java.io.*;
 public class MainClass {
 
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Test");
 		System.out.println("test 2");
-	}
 
+		try {
+			RandomAccessFile raf = new RandomAccessFile("hello.txt", "rw");
+			raf.writeBytes("Hello, world!\n");
+			raf.close();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
